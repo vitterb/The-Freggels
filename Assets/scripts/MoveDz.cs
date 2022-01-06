c8b5aa00c5a3722fa8cf2ec1d725d383458369c0
Ali <s116100@ap.be>
2022-01-06 22:14:10 +0100

    first push
    landlevel

---
+++ Assets/scripts/MoveDz.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveDz : MonoBehaviour
+{
+    public float speed = 2;
+    void Start()
+    {
+    }
+    public void Update()
+    {
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
+    }
+}
---
+++ Assets/scripts/PlayerFootStep.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFootStep : MonoBehaviour
+{
+    AudioSource animSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animSound = GetComponent<AudioSource>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void PlayerStepSound()
+    {
+        animSound.Play();
+    }
+}
