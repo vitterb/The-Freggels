897e3ef0dbf4c310bb2db2f18a722d1f67612d24
Bert <bert.vanitterbeeck@student.ap.be>
2022-01-21 13:03:28 +0100

    Added background sound to airlevel

---
+++ Assets/Scripts/Audiomanager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audiomanager : MonoBehaviour
+{
+    public AudioSource BackgroundMusic;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeMusic ( AudioClip music)
+    {
+        BackgroundMusic.Stop();
+        BackgroundMusic.clip = music;
+        BackgroundMusic.Play(); 
+    }
+}
---
+++ Assets/Scripts/Switchmusic.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switchmusic : MonoBehaviour
+{
+    public AudioClip newTrack;
+    private Audiomanager am;
+    void Start()
+    {
+        am = FindObjectOfType<Audiomanager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (newTrack != null)
+        {
+            am.ChangeMusic(newTrack);
+        }
+
+    }
+    //    Sorrow by Alexander Nakarada | https://www.serpentsoundstudios.com
+    //Music promoted by https://www.chosic.com/free-music/all/
+    //Creative Commons CC BY 4.0
+    //https://creativecommons.org/licenses/by/4.0/
+
+}
